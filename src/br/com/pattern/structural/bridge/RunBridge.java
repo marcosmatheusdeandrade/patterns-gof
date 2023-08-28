6dca57dd171a92246592627642a1ff637d513823
Marcos Matheus De Andrade <marcosmatheusad@gmail.com>
2023-08-27 22:02:13 -0300

    Ajuste em exemplo do bridge

--- src/br/com/pattern/structural/bridge/RunBridge.java
+++ src/br/com/pattern/structural/bridge/RunBridge.java
@@ -4,13 +4,19 @@ import br.com.pattern.structural.bridge.dao.ProductMongoDAO;
 import br.com.pattern.structural.bridge.dao.ProductSqlServerDAO;
 import br.com.pattern.structural.bridge.model.Product;
 import br.com.pattern.structural.bridge.service.ProductEJBService;
+import br.com.pattern.structural.bridge.service.ProductSpringService;
 
 public class RunBridge {
 
     public static void main(String[] args) {
 
         //realizada ponte dinâmica entre services e daos
-        new ProductEJBService(new ProductMongoDAO()).save(new Product("Carrinho controler remoto", 80.00));
-        new ProductEJBService(new ProductSqlServerDAO()).save(new Product("Carrinho controler remoto", 80.00));
+        new ProductEJBService(new ProductMongoDAO()).save(new Product("Carrinho controle remoto", 80.00));
+        System.out.println("---------------------------------------------------------------------------------------------------");
+
+        new ProductSpringService(new ProductSqlServerDAO()).save(new Product("Carrinho controle remoto", 80.00));
+        System.out.println("---------------------------------------------------------------------------------------------------");
+
+        new ProductEJBService(new ProductSqlServerDAO()).save(new Product("Boneco do Batman", 80.00));
     }
 }
